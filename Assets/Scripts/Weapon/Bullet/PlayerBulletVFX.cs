b0c6872b02de745b05e5fe850f961f9704851d81
SANGWOOSEE <washble@devdeopt.co.kr>
2025-07-13 19:12:02 +0900

    Add Orb VFX and Bullet Settings

---
+++ Assets/Scripts/Weapon/Bullet/PlayerBulletVFX.cs
@@ -0,0 +1,38 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PlayerBulletVFX : MonoBehaviour
+{
+    [SerializeField] private VisualEffect bulletVFX;
+    
+    [SerializeField] private AnimationCurve sizeAnimationCurve;
+    [SerializeField] private float sizeSpeed = 1;
+
+    private const string Size = "Orb Size";
+
+    private void Awake()
+    {
+        if(!bulletVFX) { bulletVFX = GetComponent<VisualEffect>(); }
+    }
+
+    private void OnEnable()
+    {
+        LifetimeSize().Forget();
+    }
+
+    private async UniTaskVoid LifetimeSize()
+    {
+        float time = 0;
+        
+        while (time < 1 && gameObject.activeInHierarchy)
+        {
+            time += Time.deltaTime * sizeSpeed;
+            float size = sizeAnimationCurve.Evaluate(time);
+            Debug.Log(size);
+            bulletVFX.SetFloat(Size, size);
+
+            await UniTask.Yield(destroyCancellationToken);
+        }
+    }
+}
